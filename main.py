59269c866b93859463086595ada3847bf59e7d1e
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2016-11-01 12:10:45 +0100

    minor visual editing

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from enum import Enum
 
 
 def registreren():
-    uID = randomID()
+    u_id = randomID()
     name = get_input("Typ uw volledige naam in: ", "^[a-zA-Z. ]+$")
     street = get_input("Typ uw straatnaam in: ", "^[a-zA-Z. ]+$")
     house_number = get_input("Typ uw huisnummer in: ", "^\d+$")
     postal_code = get_input("Typ uw postcode in: ", "^\d{4}\w{2}$")
     telnr = get_input("Typ uw telefoonnummer in: 06-", "^\d{8}$")
     securitycode = send_msg_to_nr(telnr)
-    while input("Heeft u de code ontvangen? Y/N: ") == 'N':
+    while input("Heeft u de code ontvangen? Y/N: ") == 'N':  # check to ensure user got a message on his phone
         telnr = get_input("Typ uw telefoonnummer in: 06-", "^\d{8}$")
         securitycode = send_msg_to_nr(telnr)
     while securitycode != input("Voer de code in die naar uw telefoon gestuurd is: "):  # Security code invalid
         securitycode = send_msg_to_nr(telnr)
         print("Code klopt niet. Er wordt een nieuwe verstuurd.")
     print("Code klopt. Registratie voltooid.")
-    user = {'uid': uID,
+    user = {'uid': u_id,
             'name': name,
             'street': street,
             'house_number': house_number,
@@ -31,10 +31,11 @@ def registreren():
 def get_input(console_output, pattern):
     cmd_input = ""
     try:
-        cmd_input = input(console_output)  # Eval throws an exception when cmd_input is a string
+        cmd_input = input(console_output)
         while not re.match(pattern, cmd_input):
             cmd_input = input("De invoer klopt niet. " + console_output)
-        cmd_input = eval(cmd_input)
+        cmd_input = eval(cmd_input)  # Eval throws an exception when cmd_input is a string, that's why we're using try.
+    # Eval is only done at the end because we need the input as a string at first to successfully match it with a regex.
     except:
         pass
     return cmd_input
@@ -44,7 +45,7 @@ def send_msg_to_nr(tel_nr):  # Sends an authentication message to the given numb
     code = make_random_code(6, CodeType.ALL)
     # TELEGRAM SEND MESSAGE WITH 'code' IN IT RIIIIIIIIGHT HEEEEEEEEEEEEEEEERE
     print(code)
-    #INSTEAD, IMMA USE THIS PRINT IN THE MEANWHILE
+    # INSTEAD, IMMA USE THIS PRINT IN THE MEANWHILE
     return code
 
 
@@ -69,7 +70,7 @@ def make_random_code(length, type_of_code):
     random_code = ""
     for i in range(0, length):
         random_int = random.randint(0, len(randomizer) - 1)
-        random_code = random_code + randomizer[random_int]
+        random_code += randomizer[random_int]
     return random_code
 # TEST CODE HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEERE::::
 registreren()
