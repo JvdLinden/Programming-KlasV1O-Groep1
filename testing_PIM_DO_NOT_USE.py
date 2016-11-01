0232fa90a1b7c3ec69a6c33ec9168283f7db06fa
pimvanhespen <pimvanhespen@gmail.com>
2016-11-01 14:16:50 +0100

    telegramHandler class added see basic exmaple in testing_PIM_DO_NOT_USE.py

--- testing_PIM_DO_NOT_USE.py
+++ testing_PIM_DO_NOT_USE.py
@@ -1,52 +1,74 @@
-import DatabaseHandler
+# import DatabaseHandler
+#
+# # testing Dictionary
+# myDict = {
+#     'name':'naam1',
+#     'street':'straat2',
+#     'house_number':'11',
+#     'house_number_extra':'a1',
+#     'city':'stad1',
+#     'postal_code':'1243DB',
+#     'phone_number':12323678
+# }
+#
+# #testing Table
+# myTable = 'users'
+#
+# myDB = DatabaseHandler.DatabaseHandler('test.db')
+#
+# myDB.runQuery('''
+# CREATE TABLE IF NOT EXISTS users
+#     (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT NOT NULL,
+#         street TEXT NOT NULL,
+#         house_number INT NOT NULL,
+#         house_number_extra TEXT,
+#         city TEXT NOT NULL,
+#         postal_code TEXT NOT NULL,
+#         phone_number INTEGER NOT NULL
+#     )
+# ''')
+#
+# result = myDB.insertNewItem(myDict,myTable)
+#
+# print(result)
+#
+# myDict = {
+#     'neem':'naam1',
+#     'street':'straat2',
+#     'house_number':'11',
+#     'house_number_extra':'a1',
+#     'city':'stad1',
+#     'postal_code':'1243DB',
+#     'phone_number':12323678
+# }
+# myDB.insertNewItem(myDict,myTable)
+# print(myDB.showTableContents(myTable))
+#
+# del myDB
+#
+#
+#
 
-# testing Dictionary
-myDict = {
-    'name':'naam1',
-    'street':'straat2',
-    'house_number':'11',
-    'house_number_extra':'a1',
-    'city':'stad1',
-    'postal_code':'1243DB',
-    'phone_number':12323678
-}
+#TELEGRAM TESTSITE
+import telegramHandler
 
-#testing Table
-myTable = 'users'
+token = '166998703:AAGvk0No3abHbGc9LpUpJko7WTx1DR9jURY'
 
-myDB = DatabaseHandler.DatabaseHandler('test.db')
+myTelegramHandler = telegramHandler.TelegramHandler(token, 0)
 
-myDB.runQuery('''
-CREATE TABLE IF NOT EXISTS users
-    (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        street TEXT NOT NULL,
-        house_number INT NOT NULL,
-        house_number_extra TEXT,
-        city TEXT NOT NULL,
-        postal_code TEXT NOT NULL,
-        phone_number INTEGER NOT NULL
-    )
-''')
+while True:
+    newUpdate = myTelegramHandler.getNextUpdate()
+    if len(newUpdate) > 0:
+        dictList = myTelegramHandler.handleUpdates(newUpdate)
 
-result = myDB.insertNewItem(myDict,myTable)
+        #Verwerk nieuwe codes
+        for item in dictList:
+            for key in item:
+                print('{}{}'.format(key, item[key]), end=' ,')
 
-print(result)
+            print()
+            #REGISTER CODE SOMEWHERE
 
-myDict = {
-    'neem':'naam1',
-    'street':'straat2',
-    'house_number':'11',
-    'house_number_extra':'a1',
-    'city':'stad1',
-    'postal_code':'1243DB',
-    'phone_number':12323678
-}
-myDB.insertNewItem(myDict,myTable)
-print(myDB.showTableContents(myTable))
-
-del myDB
-
-
-
+            myTelegramHandler.sendMessageToUser(item['id'], item['code'])
