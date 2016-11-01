08161c956c7fe5cee8a7f5aa2a776ecc647f8f3f
Joren van der Linden <jorenvanderlinden@live.nl>
2016-11-01 15:43:26 +0100

    Popup window toegevoegd

--- GUI.py
+++ GUI.py
@@ -1,8 +1,21 @@
 from tkinter import *
 
 
+def sub_personal_code():
+    #Default subscreen settings
+    sub_window = Toplevel(master=registration_window)
+    sub_window.title('Popup')
+
+    #Widget creation
+    code_label = Label(sub_window, text='Voer de ontvangen code in: ').grid(row=0, sticky=W)
+    code_entry = Entry(sub_window,).grid(row=0, column=1)
+
+    submit_button = Button(sub_window, text='submit').grid(row=1, column=1)
+
+
 def main():
     # Default settings:
+    global registration_window
     registration_window = Tk()
     registration_window.title('NS Fietsenstalling')
 
@@ -22,8 +35,10 @@ def main():
     phone_nr_label = Label(registration_window, text='Tel.nummer: ').grid(row=4, sticky=W)
     phone_nr_entry = Entry(registration_window).grid(row=4, column=1)
 
-    submit_button = Button(registration_window, text='submit').grid(row=6, column=1)
+    submit_button = Button(registration_window, text='submit', command=sub_personal_code).grid(row=6, column=1)
 
     # Init registration_window
     registration_window.mainloop()
+
 main()
+
