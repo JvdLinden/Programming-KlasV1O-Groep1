e8e4b0d3f5b0cddbb431a0756439334180d81fde
Joren van der Linden <jorenvanderlinden@live.nl>
2016-11-01 15:10:11 +0100

    Begint gemaakt aan start screen

---
+++ Start_Screen.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+message = Label(master=root, text='Welkom bij de fietsenstalling! \n'
+                                  'Kies één van de volgende opties.')
+message.pack()
+
+root.mainloop()
