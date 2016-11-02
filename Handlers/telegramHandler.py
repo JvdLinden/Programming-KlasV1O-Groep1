59bda3b405b57ab4d6f9e79c541e453cdf39ecf1
pimvanhespen <pimvanhespen@gmail.com>
2016-11-02 11:29:14 +0100

    hasNewUpdates method added to just check if there are any updates available

--- Handlers/telegramHandler.py
+++ Handlers/telegramHandler.py
@@ -48,6 +48,12 @@ class TelegramHandler(object):
 
         return _update
 
+    def hasNewUpdates(self):
+        _newUpdate = self.bot.getUpdates(self.current_response+1, limit=1, offset=1)
+        if len(_newUpdate) > 0:
+            return True
+        return False
+
     def handleUpdates(self, updates):
         """Handles all updates, and returns a list of dicts with update data
 
