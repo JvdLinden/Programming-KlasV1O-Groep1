cbda6f7c090516b68341fdad99170a816c47e799
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2016-11-02 13:05:10 +0100

    updated GUI a little

--- GUI.py
+++ GUI.py
@@ -45,18 +45,20 @@ def check_code(input_code, code_window):  # checks Telegram sent code to see if 
         pass
 
 
-def check_entries():
-    global data
-    for entry in entries:
-        data.append(entry.get())  # All the data is gathered into the entry array
-    res = registration.is_valid(data)  # res[0] being true means the entries are valid.
-    # res[1] contains which entry is invalid should this not be the case.
-    if res[0]:  # With our data being valid we start a pop-up to ask for a security code sent to your phone.
+def check_entries(entries):
+    # Data starts out as dict with entries
+    name = entries['name'].get()  # All the entries are converted to the input strings
+    street = entries['street'].get()
+    house_number = entries['house_number'].get()
+    postal_code = entries['postal_code'].get()
+    phone_number = entries['phone_number'].get()
+
+    if True:  # With our data being valid we start a pop-up to ask for a security code sent to your phone.
         global security_code
-        security_code = registration.send_msg_to_nr(entries[Info.PHONE_NR.value])
+        security_code = registration.send_msg_to_nr(phone_number)
         sub_personal_code()
     else:
-        sub_incorrect_data(res[1])
+        sub_incorrect_data()
 
 
 def registration_init():
@@ -66,17 +68,34 @@ def registration_init():
     registration_window.title("NS Fietsenstalling")
 
     # widget creation
-    for i, label in enumerate(labels):  # labels is a string array here,
+    name_label = Label(registration_window, text='Naam: ').grid(row=0, sticky=W)
+    name_entry = Entry(registration_window)
+    name_entry.grid(row=0, column=1)
 
-        label = Label(registration_window, text=label)
-        label.grid(row=i, sticky=W)
-        labels[i] = label  # but becomes a Label array here.
-        textbox = Entry(registration_window)
-        textbox.grid(row=i, column=1)
-        entries.append(textbox)  # entries is created as an Entry array.
+    street_label = Label(registration_window, text='Straat: ').grid(row=1, sticky=W)
+    street_entry = Entry(registration_window)
+    street_entry.grid(row=1, column=1)
 
+    house_nr_label = Label(registration_window, text='Huisnummer: ').grid(row=2, sticky=W)
+    house_nr_entry = Entry(registration_window)
+    house_nr_entry.grid(row=2, column=1)
+
+    postal_code_label = Label(registration_window, text='Postcode: ').grid(row=3, sticky=W)
+    postal_code_entry = Entry(registration_window)
+    postal_code_entry.grid(row=3, column=1)
+
+    phone_nr_label = Label(registration_window, text='Tel.nummer: ').grid(row=4, sticky=W)
+    phone_nr_entry = Entry(registration_window)
+    phone_nr_entry.grid(row=4, column=1)
+
+    user_dict = {'uid': registration.randomID(),
+                 'name': name_entry,
+                 'street': street_entry,
+                 'house_number': house_nr_entry,
+                 'postal_code': postal_code_entry,
+                 'phone_number': phone_nr_entry}
     # When the submit button is clicked, entries are checked for validity.
-    submit_button = Button(registration_window, text="submit", command=check_entries).grid(row=5, column=1, sticky=E)
+    submit_button = Button(registration_window, text="submit", command=check_entries(user_dict)).grid(row=5, column=1, sticky=E)
 
     # Init registration_window
     registration_window.mainloop()
---
+++ Validate.py
@@ -0,0 +1,5 @@
+import re
+
+
+def naam(gui_input):
+    return re.match("^[\p{L}\s.']+$", self)
