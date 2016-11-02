ee185c4da6f7c36bb8dc36d6efa6800c9633e1b0
pimvanhespen <pimvanhespen@gmail.com>
2016-11-02 12:45:19 +0100

    Added comments, changed some stuff

--- Start_Screen.py
+++ Start_Screen.py
@@ -1,22 +1,41 @@
 from tkinter import *
 
-#Function for starting Home screen
-def start_screen():
+def startScreen():
+    """Create the GUI for the main screen.
+
+    :return: returns a GUI screen
+    """
+    # Create a root screen for the GUI to add items into
     root = Tk()
+
+    # Assign a title to the screen
     root.title('Fietsenstalling beheer')
 
-    #Welcome message
+    # Welcome message
     message = Label(master=root, text='Welkom bij de fietsenstalling! \n'
                                   'Kies één van de volgende opties.').grid(row=0, column=0)
 
-    #Buttons to differente functions
-    #Commands not made yet
-    reg_button = Button(master=root, text='Registeren').grid(row=1, column=0)
-    stallen_button = Button(master=root, text='Stallen').grid(row=2, column=0)
-    ophalen_button = Button(master=root, text='Ophalen').grid(row=3, column=0)
-    alg_info_button = Button(master=root, text='Algemene Informatie').grid(row=4, column=0)
-    pers_info_button = Button(master=root, text='Persoonlijke Informatie').grid(row=5, column=0)
+    # Adding 'Registreren'-button
+    # ToDo : Add command to button
+    buttonRegister = Button(master =root, text='Registeren').grid(row=1, column=0)
 
-    root.mainloop()
+    # Adding 'Stallen'-button
+    # ToDo : Add command to button
+    buttonStore = Button(master=root, text='Stallen').grid(row=2, column=0)
 
-start_screen()
+    # Adding 'Ophalen'-button
+    # ToDo : Add command to button
+    buttonRetrieve = Button(master=root, text='Ophalen').grid(row=3, column=0)
+
+    # Adding 'Algemene Informatie'-button
+    # ToDo : Add command to button
+    buttonInfo = Button(master=root, text='Algemene Informatie').grid(row=4, column=0)
+
+    # Adding 'Persoonlijke Informatie'-button
+    # ToDo : Add command to button
+    buttonPersonalInfo = Button(master=root, text='Persoonlijke Informatie').grid(row=5, column=0)
+
+    return root
+
+screen = startScreen()
+screen.mainloop()
