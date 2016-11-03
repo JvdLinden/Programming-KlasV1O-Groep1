3516a70bc1f731da173a029b985eac720efa566d
pimvanhespen <pimvanhespen@gmail.com>
2016-11-03 12:21:38 +0100

    goede merge....

--- GUI.py
+++ GUI.py
@@ -1,48 +1,83 @@
-from Algemene_info import *
+from AlgemeneInfo import *
 from Handlers import databaseHandler, telegramHandler
 from ProjectData import Constants
 import registration
 
-def startScreen(database):
-    """Create the GUI for the main screen.
+class GUI(object):
 
-    :return: returns a GUI screen
-    """
-    # Create a root screen for the GUI to add items into
-    root = Tk()
+    def __init__(self, database, telegram):
+        """Create the GUI for the main screen.
 
-    # Assign a title to the screen
-    root.title('Fietsenstalling beheer')
+        :return: an GUI object
+        """
+        self.database = database
+        self.telegram = telegram
 
-    # Welcome message
-    message = Label(master=root, text='Welkom bij de fietsenstalling! \n'
-                                  'Kies één van de volgende opties.').grid(row=0, column=0)
+        # Create a root screen for the GUI to add items into
+        self.root = Tk()
 
-    # Adding 'Registreren'-button
-    buttonRegister = Button(master =root, text='Registeren', command=lambda: registration.registration_init(database))
-    buttonRegister.grid(row=1, column=0)
+        # Assign a title to the screen
+        self.root.title('Fietsenstalling beheer')
 
-    # Adding 'Stallen'-button
-    # ToDo : Add command to button
-    buttonStore = Button(master=root, text='Stallen').grid(row=2, column=0)
+        # Welcome message
+        self.message = Label(
+            master=self.root,
+            text='Welkom bij de fietsenstalling! \nKies één van de volgende opties.'
+        )
+        self.message.grid(row=0, column=0)
 
-    # Adding 'Ophalen'-button
-    # ToDo : Add command to button
-    buttonRetrieve = Button(master=root, text='Ophalen').grid(row=3, column=0)
+        # Adding 'Registreren'-button
+        self.buttonRegister = Button(
+            master=self.root,
+            text='Registeren',
+            command=lambda: registration.registration_init(database)
+        )
+        self.buttonRegister.grid(row=1, column=0)
 
-    # Adding 'Algemene Informatie'-button
-    buttonInfo = Button(master=root, text='Algemene Informatie', command=algemene_info).grid(row=4, column=0)
+        # Adding 'Stallen'-button
+        # ToDo : Add command to button
+        self.buttonStore = Button(
+            master=self.root,
+            text='Stallen'
+        )
+        self.buttonStore.grid(row=2, column=0)
 
-    # Adding 'Persoonlijke Informatie'-button
-    # ToDo : Add command to button
-    buttonPersonalInfo = Button(master=root, text='Persoonlijke Informatie').grid(row=5, column=0)
+        # Adding 'Ophalen'-button
+        # ToDo : Add command to button
+        self.buttonRetrieve = Button(
+            master=self.root,
+            text='Ophalen'
+        )
+        self.buttonRetrieve.grid(row=3, column=0)
 
-    return root
+        # Adding 'Algemene Informatie'-button
+        self.buttonInfo = Button(
+            master=self.root,
+            text='Algemene Informatie',
+            command=algemene_info
+        )
+        self.buttonInfo.grid(row=4, column=0)
+
+        # Adding 'Persoonlijke Informatie'-button
+        # ToDo : Add command to button
+        self.buttonPersonalInfo = Button(
+            master=self.root,
+            text='Persoonlijke Informatie'
+        )
+        self.buttonPersonalInfo.grid(row=5, column=0)
+
+    def start(self):
+        self.root.mainloop()
+
+    def stop(self):
+        self.root.destroy()
+
 
 myDatabaseHandler = databaseHandler.DatabaseHandler(Constants.DATABASE)
+myTelegramHandler = telegramHandler.TelegramHandler(Constants.BOT_TOKEN)
 
-screen = startScreen(myDatabaseHandler)
-screen.mainloop()
+screen = GUI(database=databaseHandler, telegram=myTelegramHandler)
+screen.start()
 
 
 
