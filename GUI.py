a62e753f6358b8875102d120c21643fb80545340
pimvanhespen <pimvanhespen@gmail.com>
2016-11-03 11:54:28 +0100

    replaced code, deleted obsolete code (wih Dylan)

--- GUI.py
+++ GUI.py
@@ -1,102 +1,5 @@
-from tkinter import *
-from userValidator import UserValidator
 from Algemene_info import *
 import registration
-import Validate
-
-
-# Array containing labels for registration
-labels = ["Naam: ", "Straat: ", "Huisnummer: ", "Postcode: ", "Tel.nummer: "]
-# CONSTANTS
-TELEGRAM_BOT_NAME = '@Fietsenstalling_Beheer_Bot'
-
-
-# Todo - Add comments / documentation
-def sub_personal_code(confirmation_code):
-    """
-    A pop-up containing a personal code for the user which they have to text to Telegram.
-    :param confirmation_code: The code in question
-    :return:
-    """
-    # Default subscreen settings
-    sub_window = Toplevel(master=registration_window)
-    sub_window.lift()
-    sub_window.title("Popup")
-
-    # Widget creation
-    code_label = Label(sub_window, text="Voer de ontvangen code in: ").grid(row=0, sticky=W)
-    code_entry = Entry(sub_window,)
-    code_entry.grid(row=0, column=1)
-    submit_button = Button(sub_window, text="submit")
-    submit_button.grid(row=1, column=1)
-
-
-# Todo - Add comments / documentation
-def sub_incorrect_data(incorrect_entry):
-    """
-    A pop-up to indicate to the user the registration process can't continue because of incorrect data.
-    :param incorrect_entry: The Dutch name of the entry in question containing incorrect data
-    :return: none
-    """
-    sub_window = Toplevel(master=registration_window)
-    sub_window.title("Incorrecte invoer")
-    sub_window.lift()
-
-    incorrect_label = Label(sub_window, text="De invoer is incorrect. Voer uw %s in." % incorrect_entry)
-    ok_button = Button(sub_window, text="OK", command=sub_window.destroy)
-    incorrect_label.pack()
-    ok_button.pack()
-
-
-# Todo - Add comments / documentation
-def check_entries(entries):
-    """
-        Method validates entries and throws a pop-up when something isn't noted properly.
-    :param entries: Data starts out as dictionary containing entries
-    :return: no return value.
-    """
-    # All the entries are converted to individual input strings.
-    name = entries['name'].get()  # This his hardcoded because the data type can vary.
-    street = entries['street'].get()
-    house_number = entries['house_number'].get()
-    postal_code = entries['postal_code'].get()
-    phone_number = entries['phone_number'].get()
-    # Here we start validating our data. Using this construction means we don't validate the rest of the data if,
-    # for example, the first variable is invalid. Instead it immediately jumps to a pop-up stating which entry is wrong.
-    if not Validate.string(name):
-        sub_incorrect_data('naam')
-    elif not Validate.string(street):
-        sub_incorrect_data('straat')
-    elif not Validate.huisNr(house_number):
-        sub_incorrect_data('huisnummer')
-    elif not Validate.postcode(postal_code):
-        sub_incorrect_data('postcode')
-    elif not Validate.telNr(phone_number):
-        sub_incorrect_data('telefoonnummer')
-    else:
-      # With our data being valid we start a pop-up containing a security code to send to Telegram.
-        sub_personal_code(registration.create_confirmation_code())
-        add_to_database()
-
-
-def add_to_database(database, data):
-    """
-
-    :param database: instance of a database handler
-    :param data: dictionary containing relevant data for registration
-    :return: whether the method succeeded or not
-    """
-    '''
-    This block of code will be the database transition
-        {'sticker': randomID(),
-            'name': data['name'],
-            'street': data['street'],
-            'house_number': data['house_number'],
-            'postal_code': data['postal_code'],
-            'phone_number': data['phone_number']}
-    '''
-    return False
-
 
 def startScreen():
     """Create the GUI for the main screen.
@@ -114,7 +17,8 @@ def startScreen():
                                   'Kies één van de volgende opties.').grid(row=0, column=0)
 
     # Adding 'Registreren'-button
-    buttonRegister = Button(master =root, text='Registeren', command=registration_init).grid(row=1, column=0)
+    buttonRegister = Button(master =root, text='Registeren', command=registration.registration_init)
+    buttonRegister.grid(row=1, column=0)
 
     # Adding 'Stallen'-button
     # ToDo : Add command to button
--- registration.py
+++ registration.py
@@ -1,6 +1,7 @@
-from Validate import *
+import Validate
 from ProjectData import Constants
-
+from tkinter import *
+from enum import Enum
 
 class Info(Enum):  # Class specifying in what order the information during registration is processed
     NAME = 0
@@ -8,6 +9,93 @@ class Info(Enum):  # Class specifying in what order the information during regis
     HOUSE_NR = 2
     POSTAL_CODE = 3
     PHONE_NR = 4
+
+
+# Todo - Add comments / documentation
+def sub_personal_code(confirmation_code, master):
+    """
+    A pop-up containing a personal code for the user which they have to text to Telegram.
+    :param confirmation_code: The code in question
+    :return:
+    """
+    # Default subscreen settings
+    sub_window = Toplevel(master=master)
+    sub_window.lift()
+    sub_window.title("Popup")
+
+    # Widget creation
+    code_label = Label(sub_window, text="Voer de ontvangen code in: ").grid(row=0, sticky=W)
+    code_entry = Entry(sub_window,)
+    code_entry.grid(row=0, column=1)
+    submit_button = Button(sub_window, text="submit")
+    submit_button.grid(row=1, column=1)
+
+
+# Todo - Add comments / documentation
+def sub_incorrect_data(incorrect_entry, master):
+    """
+    A pop-up to indicate to the user the registration process can't continue because of incorrect data.
+    :param incorrect_entry: The Dutch name of the entry in question containing incorrect data
+    :return: none
+    """
+    sub_window = Toplevel(master=master)
+    sub_window.title("Incorrecte invoer")
+    sub_window.lift()
+
+    incorrect_label = Label(sub_window, text="De invoer is incorrect. Voer uw %s in." % incorrect_entry)
+    ok_button = Button(sub_window, text="OK", command=sub_window.destroy)
+    incorrect_label.pack()
+    ok_button.pack()
+
+
+# Todo - Add comments / documentation
+def check_entries(entries, master):
+    """
+        Method validates entries and throws a pop-up when something isn't noted properly.
+    :param entries: Data starts out as dictionary containing entries
+    :return: no return value.
+    """
+    # All the entries are converted to individual input strings.
+    name = entries['name'].get()  # This his hardcoded because the data type can vary.
+    street = entries['street'].get()
+    house_number = entries['house_number'].get()
+    postal_code = entries['postal_code'].get()
+    phone_number = entries['phone_number'].get()
+    # Here we start validating our data. Using this construction means we don't validate the rest of the data if,
+    # for example, the first variable is invalid. Instead it immediately jumps to a pop-up stating which entry is wrong.
+    if not Validate.string(name):
+        sub_incorrect_data('naam', master)
+    elif not Validate.string(street):
+        sub_incorrect_data('straat', master)
+    elif not Validate.huisNr(house_number):
+        sub_incorrect_data('huisnummer', master)
+    elif not Validate.postcode(postal_code):
+        sub_incorrect_data('postcode', master)
+    elif not Validate.telNr(phone_number):
+        sub_incorrect_data('telefoonnummer', master)
+    else:
+      # With our data being valid we start a pop-up containing a security code to send to Telegram.
+        sub_personal_code(create_confirmation_code(), master)
+        add_to_database()
+
+
+def add_to_database(database, data):
+    """
+
+    :param database: instance of a database handler
+    :param data: dictionary containing relevant data for registration
+    :return: whether the method succeeded or not
+    """
+    '''
+    This block of code will be the database transition
+        {'sticker': randomID(),
+            'name': data['name'],
+            'street': data['street'],
+            'house_number': data['house_number'],
+            'postal_code': data['postal_code'],
+            'phone_number': data['phone_number']}
+    '''
+    return False
 
 
 def registration_init():
@@ -41,14 +129,14 @@ def registration_init():
     phone_nr_entry.insert(END, '06-')
     phone_nr_entry.grid(row=4, column=1)
 
-    user_dict = {'uid': registration.randomID(),
+    user_dict = {'uid': randomID(),
                  'name': name_entry,
                  'street': street_entry,
                  'house_number': house_nr_entry,
                  'postal_code': postal_code_entry,
                  'phone_number': phone_nr_entry}
     # When the submit button is clicked, entries are checked for validity.
-    submit_button = Button(registration_window, text="submit", command=lambda: check_entries(user_dict)).grid(row=5, column=1, sticky=E)
+    submit_button = Button(registration_window, text="submit", command=lambda: check_entries(user_dict, registration_window)).grid(row=5, column=1, sticky=E)
 
     # Init registration_window
     registration_window.mainloop()
@@ -58,7 +146,7 @@ def create_confirmation_code():
     """Creates a confirmation code to be used in Telegram verification.
     :return: the confirmation code.
     """
-    return makeRandomCode(Constants.RANDOM_CONFIRMATION_CODE_LENGTH, CodeType.ALL)
+    return Validate.makeRandomCode(Constants.RANDOM_CONFIRMATION_CODE_LENGTH, Validate.CodeType.ALL)
 
 
 # ToDo : Finish Comments / Documentation!
@@ -67,5 +155,5 @@ def randomID():
 
     :return:
     """
-    return makeRandomCode(Constants.RANDOM_ID_LENGTH, CodeType.DIGITS)
+    return Validate.makeRandomCode(Constants.LENGTH_VALIDATION_CODE, Validate.CodeType.DIGITS)
 
