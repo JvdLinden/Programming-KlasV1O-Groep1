163d09cece820104f1abadd36d58a99031a61541
Dylan-Laptop\Dylan <dylrak@hotmail.com>
2016-11-04 13:15:46 +0100

    Added docstrings

--- Validate.py
+++ Validate.py
@@ -3,31 +3,49 @@ from enum import Enum
 import string as stringImport
 import random
 
-# methods to check whether or not a string matches our specifications for that string,
+# methods to check whether or not a string matches our specifications for that string.
 
 
-# namely to use only a-z (case insensitive), periods, apostrophes and spaces.
 def string(gui_input):
+    """
+    This method checks whether or not the input string contains only a-z (case insensitive),
+    periods, apostrophes and spaces.
+    :param gui_input: the input string, taken from the gui.
+    :return: boolean of whether or not the string matches the regex.
+    """
     return re.match("[a-zA-Z.' ]+", gui_input)
 
 
-# or to use only digits
 def huisNr(gui_input):
+    """
+    This method checks whether or not the input string contains only digits.
+    :param gui_input: the input string, taken from the gui.
+    :return: boolean of whether or not the string matches the regex.
+    """
     return re.match("\d+", gui_input)
 
 
-# or to contain 4 digits, possibly a space in-between and then two letters.
 def postcode(gui_input):
+    """
+    This method checks whether or not the input string contains 4 digits, possibly a space in-between,
+     followed by two letters.
+    :param gui_input: the input string, taken from the gui.
+    :return: boolean of whether or not the string matches the regex.
+    """
     return re.match("\d{4}\s?\w{2}", gui_input)
 
 
-# or to contain 8 digits after the land code
 def telNr(gui_input):
+    """
+    This method checks whether or not the input string contains 8 digits after the Dutch land code
+    :param gui_input: the input string, taken from the gui.
+    :return: boolean of whether or not the string matches the regex.
+    """
     return re.match("06-\d{8}$", gui_input)
 
 
-# A class enumerating the different types of generatable codes
-class CodeType(Enum):
+# A class enumerating the different types of generatable codes. Used for clairvoyance in code.
+class CodeType:
     DIGITS = 0
     LETTERS = 1
     ALL = 2
